167e16f7da5104e1371485743edcd092aa650d8c
Marreta <bruno.rodrigues>
2018-08-28 23:33:29 -0300

    Classe principal

---
+++ src/main/java/br/com/hammer/controledepedidos/IsApplication.java
@@ -0,0 +1,12 @@
+package br.com.hammer.controledepedidos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IsApplication.class, args);
+	}
+}
