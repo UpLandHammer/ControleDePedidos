487d68457c78454befa322be513dcefe5037394d
Marreta <bruno.rodrigues>
2018-08-29 00:00:38 -0300

    Rest testado e pom atualizado.

---
+++ src/main/java/br/com/hammer/controledepedidos/resources/CategoriaResouce.java
@@ -0,0 +1,15 @@
+package br.com.hammer.controledepedidos.resources;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value="/categorias")
+public class CategoriaResouce {
+
+	@RequestMapping(method=RequestMethod.GET)
+	public String lista() {
+		return "Rest Ok!";
+	}
+}
