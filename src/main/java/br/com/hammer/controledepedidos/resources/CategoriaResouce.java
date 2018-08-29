20965e07116c36bb412d6d275f304ea847910899
Marreta <bruno.rodrigues>
2018-08-29 00:15:01 -0300

    Requisição via rest

--- src/main/java/br/com/hammer/controledepedidos/resources/CategoriaResouce.java
+++ src/main/java/br/com/hammer/controledepedidos/resources/CategoriaResouce.java
@@ -1,15 +1,27 @@
 package br.com.hammer.controledepedidos.resources;
 
+import java.util.List;
+import java.util.ArrayList;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import br.com.hammer.controlepedidos.domain.Categoria;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResouce {
 
 	@RequestMapping(method=RequestMethod.GET)
-	public String lista() {
-		return "Rest Ok!";
+	public List<Categoria> lista() {
+		Categoria informatica = new Categoria(1, "Informatica");
+		Categoria escritorio = new Categoria(2, "Escritorio");
+
+		List<Categoria> lista = new ArrayList<Categoria>();
+		lista.add(escritorio);
+		lista.add(informatica);
+		
+		return lista;
 	}
 }
