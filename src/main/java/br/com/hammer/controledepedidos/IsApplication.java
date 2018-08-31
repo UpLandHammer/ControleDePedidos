f68cac6eb60914f4bde50eed1b1da774ed067f37
Marreta <bruno.rodrigues>
2018-08-31 00:28:50 -0300

    Adição de entidades Estados e Cidade.

--- src/main/java/br/com/hammer/controledepedidos/IsApplication.java
+++ src/main/java/br/com/hammer/controledepedidos/IsApplication.java
@@ -8,8 +8,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import br.com.hammer.controledepedidos.domain.Categoria;
+import br.com.hammer.controledepedidos.domain.Cidade;
+import br.com.hammer.controledepedidos.domain.Estado;
 import br.com.hammer.controledepedidos.domain.Produto;
 import br.com.hammer.controledepedidos.repositories.CategoriaRepository;
+import br.com.hammer.controledepedidos.repositories.CidadeRepository;
+import br.com.hammer.controledepedidos.repositories.EstadoRepository;
 import br.com.hammer.controledepedidos.repositories.ProdutoRepository;
 
 @SpringBootApplication
@@ -20,7 +24,15 @@ public class IsApplication implements CommandLineRunner {
 
 	@Autowired
 	private ProdutoRepository produtoRepository;
+
+	@Autowired
+	private CidadeRepository cidadeRepository;
+
+	@Autowired
+	private EstadoRepository estadoRepository;
+
 	
+
 	public static void main(String[] args) {
 		SpringApplication.run(IsApplication.class, args);
 	}
@@ -33,7 +45,7 @@ public class IsApplication implements CommandLineRunner {
 		Produto pc = new Produto(null, "Computador", 2000.0 );
 		Produto printer = new Produto(null, "Impressora", 400.0 );
 		Produto mouse = new Produto(null, "Mouse", 120.0 );
-		
+
 		higiene.getProdutos().addAll(Arrays.asList(pc, printer, mouse));
 		bazar.getProdutos().addAll(Arrays.asList(printer));
 
@@ -43,5 +55,18 @@ public class IsApplication implements CommandLineRunner {
 
 		categoriaRepository.save(Arrays.asList(higiene, bazar));
 		produtoRepository.save(Arrays.asList(pc, printer, mouse));
+
+		Estado mg = new Estado(null, "Minas Gerais");
+		Estado sp = new Estado(null, "São Paulo");
+
+		Cidade bhCity = new Cidade(null, "Belo Horizonte", mg);
+		Cidade spCity = new Cidade(null, "São Paulo", sp);
+		Cidade cmpCity = new Cidade(null, "Campinas", sp);
+
+		mg.getCidades().addAll(Arrays.asList(bhCity));
+		sp.getCidades().addAll(Arrays.asList(spCity, cmpCity));
+		
+		estadoRepository.save(Arrays.asList(mg, sp));
+		cidadeRepository.save(Arrays.asList(bhCity, spCity, cmpCity));
 	}
 }
