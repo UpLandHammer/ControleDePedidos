bd81d63acc5a0a4255a14a702cff9e59ed9021e4
Marreta <bruno.rodrigues>
2018-09-04 22:35:32 -0300

    Criação de endpoint de clientes

--- src/main/java/br/com/hammer/controledepedidos/resources/ClienteResource.java
+++ src/main/java/br/com/hammer/controledepedidos/resources/ClienteResource.java
@@ -7,19 +7,20 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import br.com.hammer.controledepedidos.domain.Categoria;
-import br.com.hammer.controledepedidos.services.CategoriaService;
+import br.com.hammer.controledepedidos.domain.Cliente;
+import br.com.hammer.controledepedidos.services.ClienteService;
 
 @RestController
-@RequestMapping(value="/categorias")
-public class CategoriaResouce {
+@RequestMapping(value="/clientes")
+public class ClienteResource {
 
 	@Autowired
-	private CategoriaService service;
+	private ClienteService service;
 
 	@RequestMapping(value="/{id}", method=RequestMethod.GET)
 	public ResponseEntity<?> find(@PathVariable Integer id) {
-		Categoria categoria = service.buscar(id);
-		return ResponseEntity.ok().body(categoria);
+		Cliente cliente = service.buscar(id);
+		return ResponseEntity.ok().body(cliente);
 	}
+
 }
---
+++ src/main/java/br/com/hammer/controledepedidos/services/ClienteService.java
@@ -0,0 +1,24 @@
+package br.com.hammer.controledepedidos.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.hammer.controledepedidos.domain.Cliente;
+import br.com.hammer.controledepedidos.repositories.ClienteRepository;
+import br.com.hammer.controledepedidos.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class ClienteService {
+
+	@Autowired
+	private ClienteRepository repository;
+
+	public Cliente buscar(Integer id) {
+		Cliente dados = repository.findOne(id);
+			if(dados == null) {
+				throw new ObjectNotFoundException("Objeto não encontrado! Id:" + id 
+												+", Tipo: " + Cliente.class.getName());
+			}
+		return dados;
+	}
+}
