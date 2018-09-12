7ab9dc5f83062b96546540dc4ab5f74629d32da8
Marreta <bruno.rodrigues>
2018-09-11 23:12:37 -0300

    Criação de endpoints

---
+++ src/main/java/br/com/hammer/controledepedidos/resources/PedidoResource.java
@@ -0,0 +1,25 @@
+package br.com.hammer.controledepedidos.resources;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.hammer.controledepedidos.domain.Pedido;
+import br.com.hammer.controledepedidos.services.PedidoService;
+
+@RestController
+@RequestMapping(value="/pedidos")
+public class PedidoResource {
+
+	@Autowired
+	private PedidoService service;
+
+	@RequestMapping(value="/{id}", method=RequestMethod.GET)
+	public ResponseEntity<?> find(@PathVariable Integer id) {
+		Pedido pedido = service.buscar(id);
+		return ResponseEntity.ok().body(pedido);
+	}
+}
---
+++ src/main/java/br/com/hammer/controledepedidos/services/PedidoService.java
@@ -0,0 +1,24 @@
+package br.com.hammer.controledepedidos.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.hammer.controledepedidos.domain.Pedido;
+import br.com.hammer.controledepedidos.repositories.PedidoRepository;
+import br.com.hammer.controledepedidos.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class PedidoService {
+
+	@Autowired
+	private PedidoRepository repository;
+
+	public Pedido buscar(Integer id) {
+		Pedido dados = repository.findOne(id);
+			if(dados == null) {
+				throw new ObjectNotFoundException("Objeto não encontrado! Id:" + id 
+												+", Tipo: " + Pedido.class.getName());
+			}
+		return dados;
+	}
+}
